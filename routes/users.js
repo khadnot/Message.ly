a7cd01da77244d2ae10885c79936af0c4c3e2b25
Kenn Hadnot <k_hadnot@yahoo.com>
2021-02-10 10:59:08 -0800

    GET all users, single user, messages to, and messages from

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,37 @@
+const express = require("express");
+const router = new express.Router();const router = new express.Router();
+const User = require("../models/user");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get('/', ensureLoggedIn, async (req, res, next) => {
+    try {
+        let users = await User.all();
+        return res.json({ users });
+    } catch (e) {
+        return next(e);
+    }
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get('/:username', ensureCorrectUser, async (req, res, next) => {
+    try {
+        let user = await User.get(req.params.username);
+        return res.json({ user });
+    } catch(e) {
+        return next(e);
+    }
+});
 
 
 /** GET /:username/to - get messages to user
@@ -22,6 +44,14 @@
  *
  **/
 
+router.get('/:username/to', ensureCorrectUser, async (req, res, next) => {
+    try {
+        let messages = await User.messagesTo(req.params.username);
+        return res.json({ messages });
+    } catch(e) {
+        return next(e);
+    }
+});
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +62,12 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get('/:username/from', ensureCorrectUser, async (req, res, next) => {
+    try {
+        let messages = await User.messagesFrom(req.params.username);
+        return res.json({ messages });
+    } catch(e) {
+        return next(e);
+    }
+});
